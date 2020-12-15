aa4cdd964e304e5b863e750bdf4297de29a00e38
Fede <chiellofederico@gmail.com>
2020-12-15 17:42:40 +0100

    Init #biglietto

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,6 @@
 var nomeCognome = document.getElementById('nome').value;
 console.log(nomeCognome);
-var chilometro = document.getElementById('km').value;
-console.log(chilometro);
+var chilometri = document.getElementById('km').value;
+console.log(chilometri);
 var fasciaEta = document.getElementById('eta').value;
 console.log(fasciaEta);

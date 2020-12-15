621bff18ff5f1e1224ddc3031627ba7eb44b8f41
Fede <chiellofederico@gmail.com>
2020-12-15 17:24:10 +0100

    Init myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,6 @@
-
+var nomeCognome = document.getElementById('nome').value;
+console.log(nomeCognome);
+var chilometro = document.getElementById('km').value;
+console.log(chilometro);
+var fasciaEta = document.getElementById('eta').value;
+console.log(fasciaEta);

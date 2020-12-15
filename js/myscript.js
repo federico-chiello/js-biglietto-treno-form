bb3c40b19d0d225fcf8908de2eed989baf9c7015
Fede <chiellofederico@gmail.com>
2020-12-15 18:06:04 +0100

    informazioni biglietto passeggero

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,4 @@
+// variabili form
 var nomeCognome = document.getElementById('nome').value;
 console.log(nomeCognome);
 var chilometri = document.getElementById('km').value;
@@ -5,4 +6,5 @@ console.log(chilometri);
 var fasciaEta = document.getElementById('eta').value;
 console.log(fasciaEta);
 
+// inserimento informazioni del biglietto del passeggero
 document.getElementById('nome-passeggero').innerHTML = nomeCognome;
